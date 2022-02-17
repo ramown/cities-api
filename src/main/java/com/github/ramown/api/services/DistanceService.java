c6d8827b9be2e1d40bb2053e72674d6220e748b3
Emmanuel Ramon <emmanuelrlima@gmail.com>
2022-02-17 16:15:46 -0300

    custom exception for cities not found for distances

--- src/main/java/com/github/ramown/api/services/DistanceService.java
+++ src/main/java/com/github/ramown/api/services/DistanceService.java
@@ -1,11 +1,14 @@
 package com.github.ramown.api.services;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.ramown.api.dtos.DistanceDTO;
+import com.github.ramown.api.entities.City;
 import com.github.ramown.api.repositories.CityRepository;
 
 @Service
@@ -18,9 +21,15 @@ public class DistanceService {
 
 	public DistanceDTO distanceByPointsInMiles(Long city1, Long city2) {
 		log.info("nativePostgresInMiles ({}, {})", city1, city2);
+		
+		City obj1 = repository.findById(city1).orElseThrow(
+				() -> new EntityNotFoundException("Id not found: " + city1));
+		City obj2 = repository.findById(city2).orElseThrow(
+				() -> new EntityNotFoundException("Id not found: " + city2));
+		
 		Double distance = repository.distanceByPoints(city1, city2);
 		
-		return new DistanceDTO(repository.findById(city1).get(), repository.findById(city2).get(), distance);
+		return new DistanceDTO(obj1, obj2, distance);
 	}
 
 }
